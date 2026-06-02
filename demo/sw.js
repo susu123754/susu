35eae0ee31a78ea896e786fa6369dded754d9240
susu123754 <1063321904@qq.com>
2026-06-02 21:34:08 +0800

    Add files via upload

---
+++ demo/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'reminder-app-cache-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json'
+];
+
+// 安装Service Worker时缓存资源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 拦截请求并返回缓存
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 激活时清理旧缓存
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
